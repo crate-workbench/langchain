0a831a6a2572ccbf24ac381dc1c4e9609e34b56f
Andreas Motl <andreas.motl@crate.io>
2023-09-15 11:49:40 +0200

    CrateDB vector: Add vector store support
    The implementation is based on the `pgvector` adapter, as both PostgreSQL and
    CrateDB share similar attributes, and can be wrapped well by using the same
    SQLAlchemy layer on top.

--- libs/langchain/langchain/vectorstores/__init__.py
+++ libs/langchain/langchain/vectorstores/__init__.py
@@ -134,6 +134,12 @@ def _import_clickhouse_settings() -> Any:
     return ClickhouseSettings
 
 
+def _import_cratedb() -> Any:
+    from langchain.vectorstores.cratedb import CrateDBVectorSearch
+
+    return CrateDBVectorSearch
+
+
 def _import_dashvector() -> Any:
     from langchain.vectorstores.dashvector import DashVector
 
@@ -471,6 +477,8 @@ def __getattr__(name: str) -> Any:
         return _import_clickhouse_settings()
     elif name == "Clickhouse":
         return _import_clickhouse()
+    elif name == "CrateDBVectorSearch":
+        return _import_cratedb()
     elif name == "DashVector":
         return _import_dashvector()
     elif name == "DatabricksVectorSearch":
@@ -590,6 +598,7 @@ __all__ = [
     "Clarifai",
     "Clickhouse",
     "ClickhouseSettings",
+    "CrateDBVectorSearch",
     "DashVector",
     "DatabricksVectorSearch",
     "DeepLake",
