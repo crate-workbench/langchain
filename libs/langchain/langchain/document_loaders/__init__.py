985b303b79e1ca4cb9bf6ebbc56f4413c953bc50
Andreas Motl <andreas.motl@crate.io>
2023-09-16 20:00:09 +0200

    Add SQLAlchemy document loader

--- libs/langchain/langchain/document_loaders/__init__.py
+++ libs/langchain/langchain/document_loaders/__init__.py
@@ -155,6 +155,7 @@ from langchain.document_loaders.sitemap import SitemapLoader
 from langchain.document_loaders.slack_directory import SlackDirectoryLoader
 from langchain.document_loaders.snowflake_loader import SnowflakeLoader
 from langchain.document_loaders.spreedly import SpreedlyLoader
+from langchain.document_loaders.sqlalchemy import SQLAlchemyLoader
 from langchain.document_loaders.srt import SRTLoader
 from langchain.document_loaders.stripe import StripeLoader
 from langchain.document_loaders.telegram import (
@@ -327,6 +328,7 @@ __all__ = [
     "SlackDirectoryLoader",
     "SnowflakeLoader",
     "SpreedlyLoader",
+    "SQLAlchemyLoader",
     "StripeLoader",
     "TelegramChatApiLoader",
     "TelegramChatFileLoader",
