19a09ab821d4ee2169f9db4a9444e406939d20fa
Andreas Motl <andreas.motl@crate.io>
2024-10-28 23:26:49 +0100

    CrateDB: Refactor leftovers from langchain-core to langchain-community

--- libs/langchain/tests/data.py
+++ libs/langchain/tests/data.py
@@ -10,7 +10,3 @@ _EXAMPLES_DIR = _THIS_DIR / "integration_tests" / "examples"
 HELLO_PDF = _EXAMPLES_DIR / "hello.pdf"
 LAYOUT_PARSER_PAPER_PDF = _EXAMPLES_DIR / "layout-parser-paper.pdf"
 DUPLICATE_CHARS = _EXAMPLES_DIR / "duplicate-chars.pdf"
-
-# Paths to data files
-MLB_TEAMS_2012_CSV = _EXAMPLES_DIR / "mlb_teams_2012.csv"
-MLB_TEAMS_2012_SQL = _EXAMPLES_DIR / "mlb_teams_2012.sql"
